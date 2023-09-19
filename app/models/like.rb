2d4f963187bcb1c9a69801d0de1234ec04c9adc2
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-19 16:49:45 +0630

    Add custom methods for all the methods and test CRUD operations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_likes(count)
+    post.update(like_counter: count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def update_posts_counter(count)
+    author.increment!(:post_counter)
+  end
+
+  def recent_comments(limit = 5)
+    comments.last(limit)
+  end
 end
