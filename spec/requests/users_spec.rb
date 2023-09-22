a2c0a017a2b114ed87a06b352dfe1f07f1768a55
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-22 14:54:47 +0630

    Add tests for controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+
+    before(:each) do
+      get '/users'
+    end
+
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should return the correct status' do
+      expect(response.status).to eq(200)
+    end
+    it 'should render the correct template' do
+      expect(response).to render_template('index')
+    end
+    it 'include the correct placeholder' do
+      expect(response.body).to include('Here is a list of users')
     end
   end
 
   describe 'GET /show' do
+    before(:each) do
+      get '/users/1'
+    end
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should return the correct status' do
+      expect(response.status).to eq(200)
+    end
+    it 'should render the correct template' do
+      expect(response).to render_template('show')
+    end
+    it 'include the correct placeholder' do
+      expect(response.body).to include('Here is a user according to id')
     end
   end
 end
