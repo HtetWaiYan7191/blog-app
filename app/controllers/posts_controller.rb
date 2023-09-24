d37c984160afde826437b7a7176ba1759150614d
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-24 23:02:33 +0630

    Add view for user_post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
- def index
-   @user = User.find(params[:user_id])
-   @posts = @user.posts
+  def index
+     @user = User.find(params[:user_id])
+     @posts = @user.posts
+  end
 
- end
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
