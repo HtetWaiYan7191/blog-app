727e23575f3efd46f1dc6cd5da377a6b1822bac7
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-03 16:12:03 +0630

    Fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+  devise_for :users
   root 'users#index'
+
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[new create]
     end
   end
+
   
 end
