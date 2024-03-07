10a14735cba7bca09ba435ba868cfe7490dac253
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-03-07 16:14:16 +0630

    Implement action text and action storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+  has_rich_text :description
 
   validates :title, presence: true, allow_blank: false, length: { maximun: 250 }
   validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
